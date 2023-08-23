98806f3320671d32d8a75cef7578201c34276d09
OwenMaddox <owen@maddoxbox.com>
2023-08-23 17:42:36 -0500

    Add the wildcat color and initial layout

--- hallpass/src/App.js
+++ hallpass/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Card from "./modules/Card";
 
 function App() {
   return (
     <div className="App">
       <h1>Hall Pass</h1>
+      <Card></Card>
     </div>
   );
 }
---
+++ hallpass/src/modules/Card.js
@@ -0,0 +1,13 @@
+import "./Card.css";
+function Card() {
+  return (
+    <div class="container text-center background">
+      <div class="row">
+        <div class="col borderRight">Column</div>
+        <div class="col borderRight">Column</div>
+        <div class="col">Column</div>
+      </div>
+    </div>
+  );
+}
+export default Card;
