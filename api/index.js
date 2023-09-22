0fe759d1bc9123f4338591f2f96db4e4b6eca2d1
Wildcatcybersquad <wildcatcybersquad@gmail.com>
2023-09-22 11:36:09 -0500

    get Api saved

--- api/index.js
+++ api/index.js
@@ -14,11 +14,6 @@ app.get("/", async (req, res) => {
 
   const spreadsheetId = "1fa-1ZGBhr37gdlNaeabzcggc4nE3PKwWzyCi9Zz6s68";
 
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-
   const getRows = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
@@ -31,7 +26,7 @@ app.get("/", async (req, res) => {
     range: "Sheet1!A:C",
     valueInputOption: "USER_ENTERED",
     resource: {
-      values: [["owen", "2:04", "2:10"]],
+      values: [["Mr Jones", "2:04", "2:10"]],
     },
   });
 
@@ -39,26 +34,3 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(5050, (req, res) => console.log("5050"));
-
-//
-//
-//
-//
-// const { GoogleSpreadsheet } = require("google-spreadsheet");
-// const { promisify } = require("util");
-// const creds = require("../credentials.json");
-// async function AccessSpreadsheet() {
-//   const doc = new GoogleSpreadsheet(
-//     "1fa-1ZGBhr37gdlNaeabzcggc4nE3PKwWzyCi9Zz6s68",
-//     {
-//       apiKey: "",
-//       token: "",
-//     }
-//   );
-//   await promisify(doc.useServiceAccountAuth)(creds);
-//   const info = await promisify(doc.getInfo)();
-//   const sheet = info.worksheets[0];
-//   console.log(`Title: ${sheet.title}, Rows: ${sheet.rowCount}`);
-// }
-// AccessSpreadsheet();
-// // module.export = AccessSpreadsheet;
